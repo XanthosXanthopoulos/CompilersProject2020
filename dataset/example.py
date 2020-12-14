04a5f362f6ac542f130ffc5af206f9a08079e8c6
XanthosXanthopoulos <p3170125@aueb.gr>
2020-12-14 18:19:25 +0200

    AST implementation

--- dataset/example.py
+++ dataset/example.py
@@ -4,11 +4,14 @@
 def fib(n):    # write Fibonacci series up to n
                a = 0
                b = 1
-               while a < n:
+               while a < n :
                         print a
                         a = b
                         b = a + b
-                        c = a + (a * d + f) % 3
+                        c = a + (a * d + f) % 3 + 4 ** x
                       
 def funcwithdef(name,university="aueb"):
        print name, " studies in ", university
+
+c = a / b + x[func(a + (b * -200 + 1) ** (c + 2 * x))] ** c + dd.name()
+
